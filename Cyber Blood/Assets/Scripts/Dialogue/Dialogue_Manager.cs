7e480c9a754988df37fc244adc04b5026a403f73
Seth Reay <1017464@student.sae.edu.au>
2020-08-23 21:08:47 +1000

    Dialogue System
    Created a dialogue/text system to convey story to the player;

---
+++ Cyber Blood/Assets/Scripts/Dialogue/Dialogue_Manager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Dialogue_Manager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+
+    public Animator animator;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+        Debug.Log("End of dialogue");
+    }
+}
